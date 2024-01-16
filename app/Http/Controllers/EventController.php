6d512a1232501d3a1cfb18e2e2f0dfad240fe3be
Elna Halilovic <elna.halilovic@diamir.io>
2024-01-16 09:27:01 +0100

    change image url response, get image & category on get registeredEvents

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -71,7 +71,7 @@ class EventController extends Controller
                     'description' => $event->description,
                     'location' => $event->location,
                     'date' => $event->date,
-                    'imagePath' => Storage::url($event->image_path),
+                    'imagePath' => url('/') . Storage::url($event->image_path),
                     'category' => $event->category,
                 ];
             });
@@ -87,14 +87,12 @@ class EventController extends Controller
         $dbEvent = Event::where('event_id', $eventId)->first();
 
         if ($dbEvent) {
-            $imagePage = env('APP_URL') . ':8000' . Storage::url($dbEvent->image_path);
-
             $data = [
                 'name' => $dbEvent->name,
                 'description' => $dbEvent->description,
                 'location' => $dbEvent->location,
                 'date' => $dbEvent->date,
-                'imagePath' => $imagePage,
+                'imagePath' => url('/') . Storage::url($dbEvent->image_path),
                 'category' => $dbEvent->category
             ];
 
